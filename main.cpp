a738178dba970ab2922cfcd1efa43341311c391a
Samit Singh <samitsingh0411@gmail.com>
2024-05-09 10:43:15 +0530

    added some common patterns

--- main.cpp
+++ main.cpp
@@ -1,24 +1,41 @@
 #include "functions.h"
 
 int main() {
-	int gridsize = 78;
+	int gridsize = 40;
 	Cell** cellgrid = makegrid(gridsize);
 
+
+	//line
+	/*
 	cellgrid[4][4].buffer_state = state::alive;
-	//cellgrid[5][4].buffer_state = state::alive;
+	cellgrid[5][4].buffer_state = state::alive;
+	cellgrid[6][4].buffer_state = state::alive;*/
+
+
+	//still life box
+	/*
+	cellgrid[4][4].buffer_state = state::alive;
+	cellgrid[5][4].buffer_state = state::alive;
+	cellgrid[4][5].buffer_state = state::alive;
+	cellgrid[5][5].buffer_state = state::alive;*/
+
+
+	//spaceship
+	/*
+	cellgrid[4][4].buffer_state = state::alive;
 	cellgrid[6][4].buffer_state = state::alive;
-	//cellgrid[2][2].buffer_state = state::alive;
+	cellgrid[5][5].buffer_state = state::alive;
+	cellgrid[5][5].buffer_state = state::alive;
+	cellgrid[5][6].buffer_state = state::alive;
+	cellgrid[6][5].buffer_state = state::alive;
+	*/
 
-	cellgrid[4][4].current_state = state::alive;
-	//cellgrid[5][4].current_state = state::alive;
-	cellgrid[6][4].current_state = state::alive;
-	//cellgrid[2][2].current_state = state::alive;
-
-
+	//Game Loop
 	while (true) {
 		displaygrid(cellgrid, gridsize);
 		checkstate(cellgrid, gridsize);
 		sleep_for(100);
 		clear_console();
 	}
+
 }
